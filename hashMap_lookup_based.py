2f2e9f46aa8660c5557a1cf79306c53e3cfa3bf2
souravkap04 <skapri04@gmail.com>
2026-07-10 16:45:37 +0530

    lookup based hashmap 01

--- hashMap_lookup_based.py
+++ hashMap_lookup_based.py
@@ -59,3 +59,62 @@ def is_happy(n):
 print(is_happy(19))   # True
 print(is_happy(2))    # False       
 
+# ====================================================================================================================================
+# Q Word Pattern (LeetCode 290) 
+
+def word_pattern(pattern , s):
+    word = s.split()
+    p_to_w = {}
+    w_to_p = {}
+    if len(pattern) != len(word):
+        return False
+    for p , w in zip(pattern,word):
+        if p in p_to_w :
+            if p_to_w[p] != w:
+                return False
+        else :
+            p_to_w[p] = w
+
+        if w in w_to_p:
+            if w_to_p[w] != p:
+                return False
+        else :
+            w_to_p[w] = p
+
+    return True
+
+pattern = "abba"
+s= "dog cat cat dog"
+print(word_pattern(pattern , s))
+
+
+# ===========================================================================================================
+# Q 
+
+
+def isIsomorphic(self, s: str, t: str) -> bool:
+    lookup1 = {}
+    lookup2 = {}
+    if len(s) != len(t):
+        return False
+    for ch1 , ch2 in zip(s,t):
+        if ch1 in lookup1:
+            if lookup1[ch1] != ch2:
+                return False
+        else :
+            lookup1[ch1] = ch2
+
+
+        if ch2 in lookup2:
+            if lookup2[ch2] != ch1:
+                    return False
+
+        else :
+            lookup2[ch2] = ch1
+    
+    return True
+
+s="title"
+t = "paper"
+print(isIsomorphic(s,t))             
+
